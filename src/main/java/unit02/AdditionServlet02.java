fd5d20965e20be81aa9b49aad85076f8910bc848
co1248 <todaynownownow@gmail.com>
2021-07-09 17:03:28 +0900

    0709

---
+++ src/main/java/unit02/AdditionServlet02.java
@@ -0,0 +1,34 @@
+package unit02;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AdditionServlet02")
+public class AdditionServlet02 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		int num2 = 20;
+		int add = num1 + num2;
+		
+		request.setAttribute("num1", num1);
+		request.setAttribute("num2", num2);
+		request.setAttribute("add", add);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("addition3.jsp"); //페이지이동관련코드
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
